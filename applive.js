18f92d4e5d3284407f49cd635b3a1a2d89e7950a
Rohini Bali <rohinibalik@gmail.com>
2020-12-08 17:03:11 +0530

    clickeventhandler

--- applive.js
+++ applive.js
@@ -1,6 +1,14 @@
-// input
-var username = prompt("Give me your name?");
-//processing
-var welcomeMsg= ("This script works " + username);
-//Output
-alert(welcomeMsg);
+// // input
+// var username = prompt("Give me your name?");
+// //processing
+// var welcomeMsg= ("This script works " + username);
+// //Output
+// alert(welcomeMsg);
+
+// console.log(btnTranslate);
+
+var btnTranslate = document.querySelector("#btn-translator");
+
+btnTranslate.addEventListener("click", function clickEventhandler() {
+    console.log("Clicked!");
+});
