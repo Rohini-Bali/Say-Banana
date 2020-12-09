fa312ab4e7d8ef0586d5b51ece6aa925cf3bd454
Rohini Bali <rohinibalik@gmail.com>
2020-12-09 12:28:41 +0530

    translate input

--- applive.js
+++ applive.js
@@ -1,11 +1,31 @@
 var btnTranslate = document.querySelector("#btn-translator");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv =document.querySelector("#output");
+var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/valspeak.json"
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text
+};
+
+
+function errorHandler() {
+  console.log("error occured", error);
+  alert("something wrong with the server! try again after sometime");
+};
 
 function clickHandler() {
-  outputDiv.innerText="haha ha haha ha " + txtInput.value;
+
+  var inputText = txtInput.value; //taking input
+  //calling server for processing 
+
+  fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
+    .catch(errorHandler)
+
+  // outputDiv.innerText="haha ha haha ha " + txtInput.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler)
-
