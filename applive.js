b74e27be484d180f720cfffe47b24a7a54f78e58
Rohini Bali <rohinibalik@gmail.com>
2020-12-09 15:13:58 +0530

    button-styling

--- applive.js
+++ applive.js
@@ -2,18 +2,19 @@ var btnTranslate = document.querySelector("#btn-translator");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-var serverURL = "https://api.funtranslations.com/translate/valspeak.json"
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+// var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text) {
   return serverURL + "?" + "text=" + text
-};
+}
 
 
 function errorHandler() {
   console.log("error occured", error);
   alert("something wrong with the server! try again after sometime");
-};
+}
 
 function clickHandler() {
 
@@ -22,7 +23,10 @@ function clickHandler() {
 
   fetch(getTranslationURL(inputText))
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(json => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
     .catch(errorHandler)
 
   // outputDiv.innerText="haha ha haha ha " + txtInput.value;
